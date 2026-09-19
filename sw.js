53f9f1848e9a14de0047aa84301e68e22fd4368e
Pibody <dzhuskirill@gmail.com>
2026-09-19 16:06:21 +0300

    3D: three.js, три вида камеры, AE86 из профиля, небо, горы, деревья

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 // Офлайн-кэш: после первого открытия игра работает без сети.
-const CACHE = 'drift-v7';
-const FILES = ['./', './index.html', './manifest.json', './icon-192.png', './icon-512.png'];
+const CACHE = 'drift-v8';
+const FILES = ['./', './index.html', './manifest.json', './icon-192.png', './icon-512.png', './three.min.js'];
 self.addEventListener('install', e => { e.waitUntil(caches.open(CACHE).then(c => c.addAll(FILES)).then(() => self.skipWaiting())); });
 self.addEventListener('activate', e => { e.waitUntil(caches.keys().then(ks => Promise.all(ks.filter(k => k !== CACHE).map(k => caches.delete(k)))).then(() => self.clients.claim())); });
 self.addEventListener('fetch', e => {
