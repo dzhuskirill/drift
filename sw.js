ad213e1c1f74e8a16e5c53260461f9dda5f7f5f9
Pibody <dzhuskirill@gmail.com>
2026-09-19 09:15:42 +0300

    Занос: инерция заноса, AE86 крупнее, звук шин и мотора, евробит, своя музыка

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Офлайн-кэш: после первого открытия игра работает без сети.
-const CACHE = 'drift-v2';
+const CACHE = 'drift-v3';
 const FILES = ['./', './index.html', './manifest.json', './icon-192.png', './icon-512.png'];
 self.addEventListener('install', e => { e.waitUntil(caches.open(CACHE).then(c => c.addAll(FILES)).then(() => self.skipWaiting())); });
 self.addEventListener('activate', e => { e.waitUntil(caches.keys().then(ks => Promise.all(ks.filter(k => k !== CACHE).map(k => caches.delete(k)))).then(() => self.clients.claim())); });
