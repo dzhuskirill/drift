b9e21184f6bf66c95bf553082c8e16d1c61c21eb
Pibody <dzhuskirill@gmail.com>
2026-09-19 17:53:21 +0300

    Настоящая AE86 (Martin Trafas, CC BY 4.0), упрощённая до 40 тыс. граней

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 // Офлайн-кэш: после первого открытия игра работает без сети.
-const CACHE = 'drift-v9';
-const FILES = ['./', './index.html', './manifest.json', './icon-192.png', './icon-512.png', './three.min.js'];
+const CACHE = 'drift-v10';
+const FILES = ['./', './index.html', './manifest.json', './icon-192.png', './icon-512.png', './three.min.js', './GLTFLoader.js', './ae86.glb'];
 self.addEventListener('install', e => { e.waitUntil(caches.open(CACHE).then(c => c.addAll(FILES)).then(() => self.skipWaiting())); });
 self.addEventListener('activate', e => { e.waitUntil(caches.keys().then(ks => Promise.all(ks.filter(k => k !== CACHE).map(k => caches.delete(k)))).then(() => self.clients.claim())); });
 self.addEventListener('fetch', e => {
